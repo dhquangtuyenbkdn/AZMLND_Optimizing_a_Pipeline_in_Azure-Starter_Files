9aa44a75892cf8a6dee5a10f7dc6613ca9440427
dhquangtuyenbkdn <dhquangtuyenbkdn@gmail.com>
2022-06-15 04:05:07 +0900

    Delete appDemo directory

--- appDemo/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from . import views
-
-urlpatterns = {
-    #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.post_list, name='post_list'),
-}
